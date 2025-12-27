cabb966dbaf7f80e1a91de9673e88839431dab5b
Dmytro Vitonichenko <96179047+Scooby-Doooo@users.noreply.github.com>
2025-12-27 11:12:55 +0200

    Refactor service worker caching and fetch logic

--- sw.js
+++ sw.js
@@ -1,53 +1,20 @@
-const CACHE_NAME = 'farmer-app-v4.0';
+const CACHE_NAME = 'stellar-nexus-v1';
 const ASSETS = [
-    './',
-    './index.html',
-    './manifest.json',
-    'https://api.iconify.design/noto:game-die.svg'
+  './',
+  './index.html',
+  './style.css',
+  './game.js',
+  './manifest.json'
 ];
 
 self.addEventListener('install', (e) => {
-    e.waitUntil(
-        caches.open(CACHE_NAME).then((cache) => {
-            console.log('[Service Worker] Caching assets');
-            return cache.addAll(ASSETS);
-        })
-    );
-    self.skipWaiting(); 
-});
-
-self.addEventListener('activate', (e) => {
-    e.waitUntil(
-        Promise.all([
-            caches.keys().then((keyList) => {
-                return Promise.all(keyList.map((key) => {
-                    if (key !== CACHE_NAME) {
-                        return caches.delete(key);
-                    }
-                }));
-            }),
-            self.clients.claim()
-        ])
-    );
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
 });
 
 self.addEventListener('fetch', (e) => {
-    if (e.request.method !== 'GET' || e.request.url.includes('peerjs')) {
-        return;
-    }
-
-    e.respondWith(
-        caches.open(CACHE_NAME).then((cache) => {
-            return cache.match(e.request).then((cachedResponse) => {
-                const fetchPromise = fetch(e.request).then((networkResponse) => {
-                    if (networkResponse && networkResponse.status === 200) {
-                        cache.put(e.request, networkResponse.clone());
-                    }
-                    return networkResponse;
-                });
-
-                return cachedResponse || fetchPromise;
-            });
-        })
-    );
+  e.respondWith(
+    caches.match(e.request).then((response) => response || fetch(e.request))
+  );
 });
